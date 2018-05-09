0a3d0613bb9000caa2d2f45d3850a11a0d4237cc
Parome15 <romeromesquidapau@gmail.com>
2018-05-09 11:25:15 +0200

    Primera prova

---
+++ js.js
@@ -0,0 +1,57 @@
+function validarDni(inputDni){
+    /*
+    *   Fucion para validar el DNI.
+    *   Con sus atributos letrasValidas, dniPatron, str.   
+    */
+
+    var letrasValidas = 'TRWAGMYFPDXBNJZSQVHLCKET';
+    var dniPatron = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKET]{1}$/i;
+    var str = inputDni.toString().toUpperCase();
+
+    /* Comprueba que hay un DNI */
+    if (!dniPatron.test(str)) return false;
+
+    var dni = str
+    var letra = str.substr(-1);
+    var posicionLetras = parseInt(dni.substr(0, 8)) % 23;
+
+    /* ES la comprobacion de la letra para cada numero de DNI */
+    if (letrasValidas.charAt(posicionLetras) === letra) return true;
+
+    return false;
+}
+
+
+var app = angular.module("alumnes", []);
+app.controller("AlumnoController", 
+function ($scope) {
+    $scope.alumnes = [];
+    $scope.Insertar=function(){
+        //Validación DNI
+        if (validarDni($scope.noualumne.dni)){
+            //Insertar datos en la tabla
+            $scope.alumnos.push({
+                dni: $scope.noualumne.dni,
+                nombre: $scope.noualumne.nombre, 
+                apellido1: $scope.noualumne.apellido1, 
+                apellido2: $scope.noualumne.apellido2, 
+                email: $scope.noualumne.email, 
+                nota: $scope.noualumne.nota
+            });
+            //Reiniciar formulario
+            $scope.noualumne = null;
+            //Notificación de inserción exitosa
+            alert ("Alumne afegit.");
+        }else{
+            //Notificación de inserción fallida
+            alert("El DNI introduït no existeix.");
+        }
+        //Borrar un registro
+        $scope.eliminar = function(i){
+            $scope.alumnes.splice(i, 1);
+        }
+    }
+    
+    
+});
+
